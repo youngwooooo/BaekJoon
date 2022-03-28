56946682df83eb2603833ca0ca38acf0966ae316
youngwooooo <dlddudn02@gmail.com>
2022-03-28 22:36:27 +0900

    [단계별로 풀어보기][3. 반복문] : 별찍기 - 1

---
+++ src/step3/Exam2438.java
@@ -0,0 +1,19 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Exam2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
